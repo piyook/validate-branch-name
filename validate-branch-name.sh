f6f1f30f3334aad0c0ed9275061e7cd9b61dfaef
piyook <piyook@piyook.com>
2024-12-10 09:48:24 +0000

    initial commit

---
+++ validate-branch-name.sh
@@ -0,0 +1,14 @@
+## Validate branch name format
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+if ! [[ $BRANCH_NAME =~ ^(main|dev){1}$|^(feat|fix|hotfix|release|chore)/.+$ ]]; then
+  echo "Error: INVALID BRANCH NAME '$BRANCH_NAME': rename using format 'feature|fix|hotfix|release|core/your-branch-name'"
+  exit 1
+fi
+echo "Validated branch: $BRANCH_NAME - all OK :)"
+
+
+## you can set any regex patterns you want here such as :
+## ^(feature|fix|hotfix|release)/.+ - branch has to start with feature/, fix/, release/ or hotfix/
+## (feature|release|hotfix)/(JIRA-\d+) - it should look like feature/JIRA-1234
+## (feature|release|hotfix)/(JIRA-\d+/)?[a-z-]+ - it should look like feature/branch-name or include JIRA's code like feature/JIRA-1234/branch-name
